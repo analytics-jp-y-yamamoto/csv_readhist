cea077c7ba09ad587e721e93f01f786db7068bab
analytics-jp-y-yamamoto <129849323+analytics-jp-y-yamamoto@users.noreply.github.com>
2023-04-13 17:35:10 +0900

    Add files via upload

--- csv_readhist.py
+++ csv_readhist.py
@@ -28,41 +28,39 @@ def main_page():
     uploaded_file = st.file_uploader("ファイルを選択してください")
     #data_quantity = st.number_input("データ数:", key = "data quantity")
 
-    if uploaded_file is not None and st.button('ヒストグラム表示！'):
+    if uploaded_file is not None:
+        st.button('ヒストグラム表示！')
+        if st.button('ヒストグラム表示！'):
+            # csv読み込み
+            df0 = pd.read_csv(uploaded_file, index_col=0)
+            st.session_state.df0 = df0
 
+            st.markdown(
+                "<h1 style='text-align: center;'>ヒストグラム表示</h1>",
+                unsafe_allow_html=True,
+                )
 
+            column_list=st.session_state.df0.columns.values
+            Day_list=st.session_state.df0.index.values
+            Day_list_selector=st.sidebar.selectbox( "データセット選択",Day_list)
+            bin = st.sidebar.slider('binの幅を決めてください', 5, 100, 25)
 
-        # csv読み込み
-        df0 = pd.read_csv(uploaded_file, index_col=0)
-        st.session_state.df0 = df0
+            hist_array=[]
+            for i in range(st.session_state.df0.shape[1]):
+                hist_array.append(st.session_state.df0[column_list[i]][Day_list_selector])
 
-        st.markdown(
-            "<h1 style='text-align: center;'>ヒストグラム表示</h1>",
-            unsafe_allow_html=True,
-        )
+            Y, X, _ = plt.hist(hist_array,bins=bin, range=(-1, 1))
+            y_max = int(max(Y)) + 1
 
-        column_list=st.session_state.df0.columns.values
-        Day_list=st.session_state.df0.index.values
-        Day_list_selector=st.sidebar.selectbox( "データセット選択",Day_list)
-        bin = st.sidebar.slider('binの幅を決めてください', 5, 100, 25)
+            fig1, ax1 = plt.subplots()
 
-        hist_array=[]
-        for i in range(st.session_state.df0.shape[1]):
-            hist_array.append(st.session_state.df0[column_list[i]][Day_list_selector])
+            ax1.hist(hist_array, bins=bin, ec='navy', range=(-1, 1))
+            ax1.set_title("標準正規分布"+str(st.session_state.df0.shape[1])+"個データ")
+            ax1.set_xlabel(Day_list_selector)
+            ax1.set_ylabel("度数")
+            ax1.set_yticks(np.arange(0, y_max, int(y_max/10)))
 
-        Y, X, _ = plt.hist(hist_array,bins=bin, range=(-1, 1))
-        y_max = int(max(Y)) + 1
-
-        fig1, ax1 = plt.subplots()
-
-        ax1.hist(hist_array,bins=bin,ec='navy', range=(-1, 1))
-        ax1.set_title("標準正規分布"+str(st.session_state.df0.shape[1])+"個データ")
-        ax1.set_xlabel(Day_list_selector)
-        ax1.set_ylabel("度数")
-        ax1.set_yticks(np.arange(0,y_max,int(y_max/10)))
-
-        st.pyplot(fig1)
-
+            st.pyplot(fig1)
     else:
         st.info('☝️ CSVファイルをアップロードしてください')
 
